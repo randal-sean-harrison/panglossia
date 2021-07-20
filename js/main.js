3e398a5414bddedd6aca35b0e2675cc5a9bb34c8
Randal Sean Harrison <rharriso@nd.edu>
2021-07-20 13:09:32 -0400

    first full commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+// cypher and decypher buttons
+$("#toggle-cypher-button").on("click", function(){
+
+  if( $(".decypher").hasClass("d-none") ) {
+    $(".decypher").removeClass("d-none").css("display", "inline");
+  } else {
+    $(".decypher").addClass("d-none").css("display", "none");
+  }
+
+
+});
+
+
+
+
+
+
+});
+// document ready
