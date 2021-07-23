a537514e3aed3ba064c658f3c600e02793287dcc
Randal Sean Harrison <rharriso@nd.edu>
2021-07-23 15:58:31 -0400

    updating the codebase

--- js/main.js
+++ js/main.js
@@ -1,21 +1,39 @@
 $(document).ready(function() {
 
-// cypher and decypher buttons
-$("#toggle-cypher-button").on("click", function(){
+  // cypher and decypher buttons
+  $(".cypher").on("click", function() {
 
-  if( $(".decypher").hasClass("d-none") ) {
-    $(".decypher").removeClass("d-none").css("display", "inline");
-  } else {
-    $(".decypher").addClass("d-none").css("display", "none");
-  }
-
-
-});
+    if ($(this).next().hasClass("d-none")) {
+      $(this).next().removeClass("d-none").css("display", "inline");
+    } else {
+      $(this).next().addClass("d-none").css("display", "none");
+    }
 
 
+  });
 
 
+  // $(".decypher").on("click", function() {
+  //
+  //   var explication = $(this).data("explicate");
+  //
+  //
+  // });
 
+
+  // Set options for popovers per page with javascript objects
+  $('[data-toggle="popover"]').popover({
+    'container': 'body',
+    "animation": true,
+    "html": true,
+    "placement": 'top',
+    "trigger": 'click'
+  });
+
+  // Use this to allow popover to dismiss on clicking any element
+  $('.popover-dismiss').popover({
+    trigger: 'focus'
+  });
 
 });
 // document ready
